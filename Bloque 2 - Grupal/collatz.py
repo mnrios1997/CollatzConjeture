68e339c52cb04549c42cd0ceff1d42d9440e50e7
Matias Nahuel Rios <mnrios1997@gmail.com>
2021-02-24 12:01:40 -0300

    initial commit

---
+++ Bloque 2 - Grupal/collatz.py
@@ -0,0 +1,19 @@
+
+steps = 0
+c0 = int(input("Ingrese un numero para comprobar la Conjetura de Collatz: "))
+
+while c0 != 1:
+    if c0 % 2 == 0:
+        c0 = int(c0 / 2)
+        steps = steps +1
+        print(c0)
+        if c0 != 1:
+            continue
+    elif c0 % 2 != 0:
+        c0 = int(c0 * 3 + 1)
+        steps = steps + 1
+        print(c0)
+        if c0 != 1:
+            continue
+print("Pasos = ", steps)
+        
