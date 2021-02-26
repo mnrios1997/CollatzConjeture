5fb8a00e73e91d5efdf04c99e4721e34de3d292c
Eugenia Moreno <meugenia.moreno@gmail.com>
2021-02-26 13:58:53 -0300

    cambio contador line 8 y line 14

--- Bloque 2 - Grupal/collatz.py
+++ Bloque 2 - Grupal/collatz.py
@@ -5,13 +5,13 @@ c0 = int(input("Ingrese un numero para comprobar la Conjetura de Collatz: "))
 while c0 != 1:
     if c0 % 2 == 0:
         c0 = int(c0 / 2)
-        steps = steps +1
+        steps +=1
         print(c0)
         if c0 != 1:
             continue
     elif c0 % 2 != 0:
         c0 = int(c0 * 3 + 1)
-        steps = steps + 1
+        steps +=1
         print(c0)
         if c0 != 1:
             continue
