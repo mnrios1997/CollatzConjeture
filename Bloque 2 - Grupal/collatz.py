9b81f24873744ec392208d43235740b72d6f85b7
Rafael Moyano <rafamva@yahoo.com.ar>
2021-02-28 14:24:06 -0300

    Modificación de impresion por cada paso de cálculo. Issue #5.

--- Bloque 2 - Grupal/collatz.py
+++ Bloque 2 - Grupal/collatz.py
@@ -6,14 +6,13 @@ while c0 != 1:
     if c0 % 2 == 0:
         c0 = int(c0 / 2)
         steps +=1
-        print(c0)
+        print 'Valor: {0} - Paso: {1} '.format(c0, steps)
         if c0 != 1:
             continue
     elif c0 % 2 != 0:
         c0 = int(c0 * 3 + 1)
         steps +=1
-        print(c0)
+        print 'Valor: {0} - Paso: {1} '.format(c0, steps)
         if c0 != 1:
             continue
-print("Pasos = ", steps)
-        
+print 'Total de pasos: {0}.'.format(steps)
